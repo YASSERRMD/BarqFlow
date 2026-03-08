2f9a4f63bfce178f400652bef9905bf73833d931
YASSERRMD <arafath.yasser@gmail.com>
2026-03-08 10:47:23 +0400

    phase_18/task_2: bootstrap auth profile fetch on app init

--- web/src/main.ts
+++ web/src/main.ts
@@ -7,10 +7,17 @@ import '@vue-flow/core/dist/theme-default.css'
 import '@vue-flow/controls/dist/style.css'
 import '@vue-flow/minimap/dist/style.css'
 import App from './App.vue'
+import { useAuthStore } from './stores/auth'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+
+const authStore = useAuthStore(pinia)
+if (authStore.token) {
+    authStore.fetchMe()
+}
 
 app.mount('#app')
